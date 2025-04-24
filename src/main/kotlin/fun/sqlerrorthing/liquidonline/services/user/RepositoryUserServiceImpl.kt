c3050487516ad1939fb09672d618749f03f62910
sqlerrorthing <sqlerrorthing@gmail.com>
2025-04-24 10:08:17 +0800

    whoops

--- src/main/kotlin/fun/sqlerrorthing/liquidonline/services/user/RepositoryUserServiceImpl.kt
+++ src/main/kotlin/fun/sqlerrorthing/liquidonline/services/user/RepositoryUserServiceImpl.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class RepositoryUserService(
+class RepositoryUserServiceImpl(
     private val repository: UsersRepository
 ): UserService {
     @Transactional(readOnly = true)
