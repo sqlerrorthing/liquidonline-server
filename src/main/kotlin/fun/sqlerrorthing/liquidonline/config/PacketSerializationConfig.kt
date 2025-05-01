46ae10dec3196d1aa74f70bdfb2d0276ffef7668
sqlerrorthing <sqlerrorthing@gmail.com>
2025-05-01 22:33:35 +0800

    jackson serialization

--- src/main/kotlin/fun/sqlerrorthing/liquidonline/config/PacketSerializationConfig.kt
+++ src/main/kotlin/fun/sqlerrorthing/liquidonline/config/PacketSerializationConfig.kt
@@ -1,6 +1,7 @@
 package `fun`.sqlerrorthing.liquidonline.config
 
 import `fun`.sqlerrorthing.liquidonline.packets.strategy.PacketSerializationStrategy
+import `fun`.sqlerrorthing.liquidonline.packets.strategy.impl.jackson.json.JacksonJsonPacketSerializationStrategy
 import `fun`.sqlerrorthing.liquidonline.packets.strategy.impl.netty.compilertime.CompilerTimeByteBufPacketSerializationStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,6 +10,6 @@ import org.springframework.context.annotation.Configuration
 class PacketSerializationConfig {
     @Bean
     fun packetSerializationStrategy(): PacketSerializationStrategy {
-        return CompilerTimeByteBufPacketSerializationStrategy()
+        return JacksonJsonPacketSerializationStrategy()
     }
 }
